09073a6f9c31cd63b82f22eb72ed6e06333ba502
Richard Afro <richyafro@hotmail.com>
2019-02-17 18:34:34 +0100

    Add redux and dispatch actions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import styled from 'styled-components';
-import MyButton from '../src/components/button';
+import { store, DELETE_TWEET, LIKE_TWEET, RETWEET_TWEET } from './store';
 
 const H1 = styled.h1`
   color: blue;
@@ -17,13 +17,66 @@ const Img = styled.img`
   transform: rotateZ(90deg);
   transition: 1s ease-in;
 `;
+
+let id = 0;
 class App extends Component {
+  state = {
+    tweets: [],
+  };
+  componentDidMount() {
+    store.subscribe(() => {
+      this.setState({ tweets: store.getState().tweets });
+    });
+  }
   render() {
+    const { tweets } = this.state;
     return (
       <div className="App">
         <H1>This is LevelUp Academy</H1>
         <Img src={logo} alt="the logo" />
-        <MyButton primary>My Button</MyButton>
+        {tweets.map((tweet) => {
+          return (
+            <div>
+              <h1>{tweet.user}</h1>
+              <p>{tweet.tweet}</p>
+              <button
+                onClick={() =>
+                  store.dispatch({ type: DELETE_TWEET, payload: tweet })
+                }
+              >
+                Delete
+              </button>
+            </div>
+          );
+        })}
+        <button
+          onClick={() =>
+            store.dispatch({
+              type: LIKE_TWEET,
+              payload: {
+                id: id++,
+                tweet: 'The election was cancelled',
+                user: 'Somotoye',
+              },
+            })
+          }
+        >
+          Tweet
+        </button>
+        <button
+          onClick={() =>
+            store.dispatch({
+              type: RETWEET_TWEET,
+              payload: {
+                id: id++,
+                tweet: 'Something just happened',
+                user: 'Kingsley',
+              },
+            })
+          }
+        >
+          Retweet
+        </button>
         <div className="alert alert-success">This is an alert</div>
       </div>
     );
